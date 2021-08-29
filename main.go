35437a7f8dc0aa44bd543016e4455e517a125603
cwinters8 <clwinters8@gmail.com>
2021-08-29 12:42:59 -0500

    simplify pulumiConfig to just config

--- main.go
+++ main.go
@@ -4,12 +4,12 @@ import (
 	"pulumi-aws-hosted-zone/modules"
 
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
-	pulumiConfig "github.com/pulumi/pulumi/sdk/v3/go/pulumi/config"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi/config"
 )
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		conf := pulumiConfig.New(ctx, "")
+		conf := config.New(ctx, "")
 		domain := conf.Require("domain")
 		zone, err := modules.ConfigureHostedZone(*ctx, domain)
 		if err != nil {
